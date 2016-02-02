15a97ce5696606b5bc0ad1b3abdd25e262569e8d
Nick <mykola.veremeichyk@gmail.com>
2016-02-02 12:28:05 +0200

    Init commit

---
+++ src/document/BasicDocument.java
@@ -0,0 +1,140 @@
+package document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 
+ * A naive implementation of the Document abstract class. 
+ * @author UC San Diego Intermediate Programming MOOC team
+ */
+public class BasicDocument extends Document 
+{
+
+	private List<String> words;
+	/** Create a new BasicDocument object
+	 * 
+	 * @param text The full text of the Document.
+	 */
+	public BasicDocument(String text)
+	{
+		super(text);
+		words = getWords(text);
+	}
+	
+	
+	/**
+	 * Get the number of words in the document.
+	 * "Words" are defined as contiguous strings of alphabetic characters
+	 * i.e. any upper or lower case characters a-z or A-Z
+	 * 
+	 * @return The number of words in the document.
+	 */
+	@Override
+	public int getNumWords()
+	{
+		//TODO: Implement this method.  See the Module 1 support videos 
+	    // if you need help.
+		return words.size();
+	}
+
+	/**
+	 *
+	 * @param text
+	 * @return
+	 */
+	private List<String> getWords(String text) {
+		String[] words = text.split(" +");
+		List<String> ws = new ArrayList<>();
+		for (String s : words) {
+			if (!s.equals("")) {
+				String string = s.replaceAll("\\W", "");
+				if (!string.matches("\\d+\\.?\\d*")) {
+					ws.add(string);
+				}
+			}
+		}
+		return ws;
+	}
+
+	/**
+	 * Get the number of sentences in the document.
+	 * Sentences are defined as contiguous strings of characters ending in an 
+	 * end of sentence punctuation (. ! or ?) or the last contiguous set of 
+	 * characters in the document, even if they don't end with a punctuation mark.
+	 * 
+	 * @return The number of sentences in the document.
+	 */
+	@Override
+	public int getNumSentences()
+	{
+	    //TODO: Implement this method.  See the Module 1 support videos 
+        // if you need help.
+		String[] sentenses = getText().split("\\?+|\\.+|!+");
+		int result = 0;
+		for (String s : sentenses) {
+			if (!s.equals("")) {
+				result++;
+			}
+		}
+        return result;
+	}
+	
+	/**
+	 * Get the number of syllables in the document.
+	 * Words are defined as above.  Syllables are defined as:
+	 * a contiguous sequence of vowels, except for a lone "e" at the 
+	 * end of a word if the word has another set of contiguous vowels, 
+	 * makes up one syllable.   y is considered a vowel.
+	 * @return The number of syllables in the document.
+	 */
+	@Override
+	public int getNumSyllables()
+	{
+	    //TODO: Implement this method.  See the Module 1 support videos 
+        // if you need help.
+		int result = 0;
+		for (String word : words) {
+			result = result + countSyllables(word);
+		}
+
+        return result;
+	}
+
+	@Override
+	public double getFleschScore() {
+		{
+			int words = getNumWords();
+			int sen = getNumSentences();
+			int syll = getNumSyllables();
+
+			return 206.835 - 1.015 * (double)words/sen - 84.6 * (double)syll/words;
+		}
+	}
+
+	/* The main method for testing this class. 
+	 * You are encouraged to add your own tests.  */
+	public static void main(String[] args)
+	{
+		testCase(new BasicDocument("This is a test.  How many???  "
+		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
+				16, 13, 5);
+		testCase(new BasicDocument(""), 0, 0, 0);
+		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
+		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
+		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
+				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+				+ "the correct amount of syllables (example, for example), "
+				+ "but most of them will."), 49, 33, 3);
+		testCase(new BasicDocument("Segue"), 2, 1, 1);
+		testCase(new BasicDocument("Sentence"), 2, 1, 1);
+		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
+		         32, 15, 1);
+
+		testFlashCase(new BasicDocument("Sentence"), 36.6);
+		
+		
+	}
+	
+}
